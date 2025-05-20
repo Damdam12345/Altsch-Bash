9ee7dd53ac48a8ec2be2e10c0699e5de51dc2bce
Damilare <oadesikemi@gmail.com>
2025-05-20 22:08:51 +0100

    modified the file

--- checkfile.sh
+++ checkfile.sh
@@ -11,25 +11,14 @@ fi
 #variable assigned to the argument provided
 file=$1
 
+#search for file provided in the argument
+check=$(find ~ -type f -name "$file" 2>/dev/null)
+
 #To check if the file exist in the directory
-if [[ -e "$file" ]];
+if [[ -e "$check" ]];
 then
-  echo "file exists here: $file"
+  echo "file exists here: $check"
 else
      echo "File does not exist"
 fi
 
-#OR
-#---------------------------------------------------------------------
-
-#Prompt user to type the name of the file to search for
-#echo "Hi $USER, input the name of the file here"
-#read FILE
-
-#To check if the file exist in the directory
-#if [[ -e $FILE ]];
-#then
-#  echo "file exists"
-#else
-#     echo "File does not exist"
-#fi
